2fab602997831b43ad8f19240aa1e40fdea70149
dannabelleli <belleli.danna@gmail.com>
2023-05-13 12:01:42 -0700

    added a get info button to each movie elemnt, added an event listener to all elemnets buttons, added a function to fetch the movie from the api and then to get the information from a specific movie that was click on

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ var search = document.getElementById('search');
 //getMovies(APIURL);
 
 //waits for url request to show movie data
-async function getMovies(url){
+async function getMovies(url) {
     var resp = await fetch(url);
     var respData = await resp.json();
 
@@ -25,14 +25,15 @@ async function getMovies(url){
     return respData;
 };
 // gets movie data (poster,title,rating) from api request and creates img and info class and appends them to main on html 
-function showMovies(movies){
-    main.innerHTML='';
+function showMovies(movies) {
+    main.innerHTML = '';
     movies.forEach(movie => {
-        var {poster_path,title,vote_average}=movie;   // add overview - if we decide to use from tmdb
-    
+        var { poster_path, title, vote_average } = movie;   // add overview - if we decide to use from tmdb
+
         var movieEl = document.createElement('div');
         movieEl.classList.add('movie');
-        movieEl.innerHTML=`
+
+        movieEl.innerHTML = `
             <img 
               src="${IMGPATH + poster_path}" 
               alt="${title}"
@@ -40,32 +41,54 @@ function showMovies(movies){
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                <button class="btn-plot" data-attr="${title}">Get Info</button>
             </div>   
             `;
-            main.appendChild(movieEl);
+        main.appendChild(movieEl);
     });
+    // Adding an event listener to the get more info button
+    document.querySelectorAll(".btn-plot").forEach(element => element.addEventListener("click", getPlot));
 
 }
 
 // adds color coding to rating WIP
-function getClassByRate(vote){
- if(vote >8){
-    return 'green'
- }
- else if (vote >=5){
-    return 'orange'
- }
- else {
-    return 'red'
- }
+function getClassByRate(vote) {
+    if (vote > 8) {
+        return 'green'
+    }
+    else if (vote >= 5) {
+        return 'orange'
+    }
+    else {
+        return 'red'
+    }
 }
 // listens for submit from search bar, searchapi used to search from tmdb database
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    var searchTerm =search.value;
+    var searchTerm = search.value;
 
-    if(searchTerm){
+    if (searchTerm) {
         getMovies(SEARCHAPI + searchTerm);
-        search.value="";
+        search.value = "";
     }
 });
+
+
+// Getting the movie from the api
+var getMovie = function (title) {
+    var omdbTitleURL = `http://www.omdbapi.com/?t=${title}&apikey=1251d790`
+    fetch(omdbTitleURL)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+        })
+}
+
+// getting the relevant movie details in order to get the movie's plot
+var getPlot = function (event) {
+    event.preventDefault()
+    var title = event.target.getAttribute("data-attr")
+    console.log(title)
+    getMovie(title)
+}
