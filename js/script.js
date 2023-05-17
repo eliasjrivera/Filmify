23487f12e7e1116bebd029776e8bd51fd7ed73d8
Elias Rivera <eliasjohnrivera@gmail.com>
2023-05-16 19:35:36 -0700

    updated comments

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
 var APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=47f9f40a1d4ef8eb3fd7dc23d4bba6bf&page=1';
 
-// for show movies if we need to go this route with descriptions (possibly use this and append "learn more" wiki link)
-// <div class="overview">
-//<h3>Overview:</h3>
-//${overview}
-//</div> 
-
 var IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 var SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=47f9f40a1d4ef8eb3fd7dc23d4bba6bf&query=";
 
 var main = document.getElementById('main');
 var form = document.getElementById('form');
 var search = document.getElementById('search');
+// the variable pastSearchesArray gets items from local storage
 var pastSearchesArray =
     JSON.parse(localStorage.getItem("pastSearches"))
     || [];
@@ -23,13 +18,14 @@ var popupMovieTitle = document.getElementById('popup-movie-title');
 
 getMovies(APIURL);
 
+// variables for history section in header
 var historyButtons = document.getElementById('historyButtons');
-
 var button = document.querySelector('.button');
 
-button.addEventListener("click",clearhistory);
+// event listener for clear history button that calls the function clear history
+button.addEventListener("click", clearhistory);
 
-
+// function to clear from local storage
 function clearhistory() {
     window.localStorage.clear();
     pastSearchesArray = [];
@@ -37,24 +33,21 @@ function clearhistory() {
     console.log("click");
 };
 
-// function to create buttons from local storage history with clear history
+// function to create history info from local storage
 var renderSearchHistory = function () {
     historyButtons.textContent= '';
 
     if (pastSearchesArray.length > 3) {
         pastSearchesArray.shift()
-        // pastSearchesArray.push()
     };
 
     for (var i = 0; i < 3; i++) {
         var historyBtn = document.createElement('p');
         historyBtn.textContent = pastSearchesArray[i];
         historyBtn.classList.add('btn');
-        historyButtons.appendChild(historyBtn)
-    }
-
-// if statment, equal 5, shift to remove from begining, push from end pop unshift
-}
+        historyButtons.appendChild(historyBtn);
+    };
+};
 
 //waits for url request to show movie data
 async function getMovies(url) {
@@ -89,8 +82,7 @@ function showMovies(movies) {
     });
     // Adding an event listener to the get more info button
     document.querySelectorAll(".btn-plot").forEach(element => element.addEventListener("click", getPlot));
-
-}
+};
 
 // adds color coding to rating WIP
 function getClassByRate(vote) {
@@ -110,9 +102,9 @@ form.addEventListener("submit", (e) => {
     var searchTerm = search.value;
 
     if (searchTerm) {
-        // first step save local storage  up  
         pastSearchesArray.push(searchTerm);
-        localStorage.setItem("pastSearches", JSON.stringify(pastSearchesArray))
+        // this sets the item to local storage
+        localStorage.setItem("pastSearches", JSON.stringify(pastSearchesArray));
         getMovies(SEARCHAPI + searchTerm);
         search.value = "";
     }
@@ -131,9 +123,7 @@ var getMovie = function (title) {
             popupMovieTitle.textContent = result.Title;
             popup.classList.add('is-active');
         })
-
-
-}
+};
 
 // getting the relevant movie details in order to get the movie's plot
 var getPlot = function (event) {
