6ff9b7f50692db537dfe9bbd380f1aef6c3c6878
EgRoku <jimenezjj5150@gmail.com>
2023-05-16 19:30:34 -0700

    made clicking filmify button refresh to landing page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-var APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=47f9f40a1d4ef8eb3fd7dc23d4bba6bf&page=1';
-
-// for show movies if we need to go this route with descriptions (possibly use this and append "learn more" wiki link)
-// <div class="overview">
-//<h3>Overview:</h3>
-//${overview}
-//</div> 
+var APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=47f9f40a1d4ef8eb3fd7dc23d4bba6bf&page=1'; 
 
 var IMGPATH = 'https://image.tmdb.org/t/p/w1280';
 var SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=47f9f40a1d4ef8eb3fd7dc23d4bba6bf&query=";
